9fd01245f2a2b4095456bd7d8be3338af0ab1740
anshkush92college <imt_2020013@iiitm.ac.in>
2023-04-18 03:05:31 +0530

    Created the basic layout, able to add cousin level items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Row from './components/row/Row';
-
+import AddRow from './components/buttons/AddRow';
 import { useSelector } from 'react-redux';
 
 const App = () => {
@@ -7,6 +7,10 @@ const App = () => {
 
   return (
     <div className='flex flex-col gap-1'>
+      <div className='flex justify-between group mx-8 px-4 py-2 items-center my-10 bg-slate-300'>
+        Field Name and Type
+        <AddRow />
+      </div>
       {items.map((item, index) => (
         <Row item={item} key={index} />
       ))}
