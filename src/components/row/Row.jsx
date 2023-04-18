87abdc7ac989977af565a256c0c04fb087eda826
anshkush92college <imt_2020013@iiitm.ac.in>
2023-04-18 16:10:50 +0530

    Added the child structure
    Signed-off-by: anshkush92college <imt_2020013@iiitm.ac.in>

--- src/components/row/Row.jsx
+++ src/components/row/Row.jsx
@@ -21,7 +21,7 @@ const Row = ({ item }) => {
 
   return (
     <>
-      <div className='flex bg-gray-100 group hover:bg-gray-200 mx-8 my-1 p-4 border-b-2 border-gray-300 gap-4 items-center justify-between'>
+      <div className='flex bg-gray-100 group hover:bg-gray-200 mx-8 p-4 border-b-2 border-gray-300 gap-4 items-center justify-between'>
         <div className='flex'>
           <Field body={body} />
           <DataTypes
@@ -36,9 +36,11 @@ const Row = ({ item }) => {
         </div>
       </div>
 
-      {items?.map((item, index) => (
-        <Row item={item} key={index} />
-      ))}
+      <div className='pl-12 mt-2'>
+        {items?.map((item, index) => (
+          <Row item={item} key={index} />
+        ))}
+      </div>
     </>
   );
 };
