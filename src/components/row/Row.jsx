45c8b60f793979a2dae72e170c1da5b81af3958e
anshkush92college <imt_2020013@iiitm.ac.in>
2023-04-18 14:41:59 +0530

    Now able to render all the recursive comments in the same level, need to do that in the recursive level
    Signed-off-by: anshkush92college <imt_2020013@iiitm.ac.in>

--- src/components/row/Row.jsx
+++ src/components/row/Row.jsx
@@ -9,7 +9,7 @@ import Field from '../input/Field';
 import dataTypes from '../../constants/dataTypes';
 
 const Row = ({ item }) => {
-  const { body, dataType } = item;
+  const { body, dataType, items } = item;
 
   const [currentDataType, setCurrentDataType] = useState(
     dataType.toUpperCase()
@@ -20,20 +20,26 @@ const Row = ({ item }) => {
   };
 
   return (
-    <div className='flex bg-gray-100 group hover:bg-gray-200 mx-8 my-1 p-4 border-b-2 border-gray-300 gap-4 items-center justify-between'>
-      <div className='flex'>
-        <Field body={body} />
-        <DataTypes
-          changeCurrentDataType={changeCurrentDataType}
-          currentDataType={currentDataType}
-        />
+    <>
+      <div className='flex bg-gray-100 group hover:bg-gray-200 mx-8 my-1 p-4 border-b-2 border-gray-300 gap-4 items-center justify-between'>
+        <div className='flex'>
+          <Field body={body} />
+          <DataTypes
+            changeCurrentDataType={changeCurrentDataType}
+            currentDataType={currentDataType}
+          />
+        </div>
+        <div className='items-center hidden gap-4 group-hover:flex'>
+          <Toggle />
+          {currentDataType === dataTypes[3].toUpperCase() && <AddRow />}
+          <DeleteRow />
+        </div>
       </div>
-      <div className='items-center hidden gap-4 group-hover:flex'>
-        <Toggle />
-        {currentDataType === dataTypes[3].toUpperCase() && <AddRow />}
-        <DeleteRow />
-      </div>
-    </div>
+
+      {items?.map((item, index) => (
+        <Row item={item} key={index} />
+      ))}
+    </>
   );
 };
 
